0b7e2b5ce469cd2a2b9170502c2a06f4591c7ee1
Rokas <r.diedonis@yahoo.com>
2019-01-03 01:23:10 +0200

    root margin treshold increased

--- js/lazyLoading.js
+++ js/lazyLoading.js
@@ -752,7 +752,7 @@
         if (api_key) {
           var options = {
             rootMargin: '0px',
-            threshold: 0.75
+            threshold: 1
           }
       
           var map = document.getElementById('map')
