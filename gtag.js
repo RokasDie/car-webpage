ff91c9f9d17d4cc19b308952506a5fb5792dc2b5
Rokas <r.diedonis@yahoo.com>
2019-01-04 22:24:29 +0200

    changed google analytics account, so need to change gtag.js

--- gtag.js
+++ gtag.js
@@ -1,21 +1,9 @@
-window.dataLayer = window.dataLayer || [];
+// Global site tag (gtag.js) - Google Analytics 
+
+
+  window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-131658868-1');
-  gtag('config', 'AW-771972095');
+  gtag('config', 'UA-131749913-1');
 
-function gtag_report_conversion(url) {
-    var callback = function () {
-      if (typeof(url) != 'undefined') {
-        window.location = url;
-      }
-    };
-    gtag('event', 'conversion', {
-        'send_to': 'AW-771972095/zZbfCNqXxZIBEP-3jfAC',
-        'event_callback': callback
-    });
-    return false;
-  }
-
-  
