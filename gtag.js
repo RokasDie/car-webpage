fff938e8bd8ca533eabfbb1390b7637686e4cf30
Rokas <r.diedonis@yahoo.com>
2019-01-06 01:21:52 +0200

    added google analytics event trakcing

--- gtag.js
+++ gtag.js
@@ -6,6 +6,23 @@
   gtag('js', new Date());
 
   gtag('config', 'UA-131749913-1');
-  //conversion of contact form
+  //conversion of contact form adwords
   gtag('config', 'AW-771972095');
+
+  function gtag_report_conversion(url) {
+    var callback = function () {
+      if (typeof(url) != 'undefined') {
+        window.location = url;
+      }
+    };
+    gtag('event', 'conversion', {
+        'send_to': 'AW-771972095/zFqfCK3W15IBEP-3jfAC',
+        'event_callback': callback
+    });
+    return false;
+  }
+
+  gtag('config', 'AW-771972095/ZdUrCMDa15IBEP-3jfAC', {
+        'phone_conversion_number': '+37068845247'
+      });
 
